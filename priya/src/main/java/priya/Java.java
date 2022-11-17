1d10a7b4f0703db613700267b52b4bc9240800a2
priya <kanapriyanka@gmail.com>
2022-11-17 20:55:17 +0530

    sbehgsbczbdha

--- priya/src/main/java/priya/Java.java
+++ priya/src/main/java/priya/Java.java
@@ -1,7 +1,7 @@
 package priya;
 
 public class Java {
-static int num = 5;
+static int num = 10;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
  System.out.println(num);
